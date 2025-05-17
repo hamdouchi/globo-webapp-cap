30be1b679c7bc06e2338b75ac39e508fab71e214
HAMDOUCHI <mohamed.hamdouchi@capgemini.com>
2025-05-17 11:23:13 +0200

    Switch to using secrets manager

--- variables.tf
+++ variables.tf
@@ -32,11 +32,15 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "api_key" {
+variable "api_key_secret_id" {
   type        = string
-  description = "(Required) API key for web app to talk to SaaS platform."
+  description = "(Required) Secret ID for API key for web app to talk to SaaS platform."
 }
 
+variable "ec2_role_name" {
+  type        = string
+  description = "(Required) The role name for instance profile"
+}
 
 variable "playbook_repository" {
   type        = string
